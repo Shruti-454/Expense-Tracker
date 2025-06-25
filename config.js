cbecf06112ba38e8064266b44c4ce436f68030ce
Shruti Mahato <119997569+Shruti-454@users.noreply.github.com>
2025-06-25 18:40:54 +0000

    updated site

---
+++ config.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQhyrirIC2-IGF41f-0XOwXmXWq3mWqY8",
+    authDomain: "expense-tracker-69527.firebaseapp.com",
+    projectId: "expense-tracker-69527",
+    storageBucket: "expense-tracker-69527.firebasestorage.app",
+    messagingSenderId: "615181368621",
+    appId: "1:615181368621:web:636fc0b5a8aa6a52b9a7a4",
+    measurementId: "G-DWYJ4MWB50"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
